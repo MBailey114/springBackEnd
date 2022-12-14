71914b6ab94c7adcb6be29d713961786e170782f
MBailey114 <baileymichael114@gmail.com>
2022-12-14 11:40:47 +0000

    initial

---
+++ src/test/java/com/simplishop/SimpliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
