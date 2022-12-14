a652d61444ed2e72021efc6bbc9915b1ab54dc5b
MBailey114 <baileymichael114@gmail.com>
2022-12-14 14:42:07 +0000

    userController changes

--- src/main/java/com/simplishop/item/ItemConfig.java
+++ src/main/java/com/simplishop/item/ItemConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class itemConfig {
+public class ItemConfig {
     @Bean
     CommandLineRunner commandLineRunnerItem(
             ItemRepository itemRepository) {
