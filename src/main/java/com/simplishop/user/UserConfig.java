cd545f6bf95b73581c492eda06b20d7274d11a3a
MBailey114 <baileymichael114@gmail.com>
2022-12-15 10:30:46 +0000

    fixes

--- src/main/java/com/simplishop/user/UserConfig.java
+++ src/main/java/com/simplishop/user/UserConfig.java
@@ -2,9 +2,11 @@ package com.simplishop.user;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+@Configuration
 public class UserConfig {
     @Bean
     CommandLineRunner commandLineRunnerUser(
