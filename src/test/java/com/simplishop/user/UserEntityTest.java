d9c5a89459bba8422106f6fcdc83d6ef2962863b
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-22 12:26:33 +0000

    Updates to tests

--- src/test/java/com/simplishop/user/UserEntityTest.java
+++ src/test/java/com/simplishop/user/UserEntityTest.java
@@ -1,9 +1,13 @@
 package com.simplishop.user;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -62,5 +66,80 @@ class UserEntityTest {
         assertEquals(null, user.getEmailAddress());
         assertEquals(null, user.getWishlist());
         assertEquals(new ArrayList<>(), user.getRoles());
+    }
+
+    @DataJpaTest
+    //USING A H2 DATABASE TO SIMULATE A REAL DATABASE
+    //CREATES A DATABASE IN MEMORY
+    @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+    public static class UserRepositoryTests {
+
+        @Autowired
+        private UserRepository userRepository;
+
+    //    ATTRIBUTES FOR USER - emailAddress, firstName, lastName, password
+        @Test
+        public void  UserRepository_SaveAll_ReturnSavedUser() {
+
+            //Arrange
+            UserEntity user = UserEntity.builder()
+                    .emailAddress("bob@gmail.com")
+                    .firstName("Bob")
+                    .lastName("Ross")
+                    .password("artlover123")
+                    .build();
+
+            //Act
+            UserEntity savedUser = userRepository.save(user);
+
+            //Assert
+    //        Checking that the item created and saved in the repository is actually being saved
+            Assertions.assertThat(savedUser).isNotNull();
+            Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+        }
+
+        @Test
+        public void  UserRepository_GetAll_ReturnMoreThanOneItem() {
+
+            //Arrange
+            UserEntity user = UserEntity.builder()
+                    .emailAddress("bob@gmail.com")
+                    .firstName("Bob")
+                    .lastName("Ross")
+                    .password("artlover123")
+                    .build();
+
+            //Act
+            UserEntity savedUser = userRepository.save(user);
+
+            //Assert
+    //        Checking that the item created and saved in the repository is actually being saved
+            Assertions.assertThat(savedUser).isNotNull();
+            Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+        }
+
+        @Test
+        public void UserRepository_FindById_ReturnsUser(){
+            //Arrange
+            UserEntity user = UserEntity.builder()
+                    .emailAddress("bob@gmail.com")
+                    .firstName("Bob")
+                    .lastName("Ross")
+                    .password("artlover123")
+                    .build();
+
+            //Act
+            userRepository.save(user);
+
+            UserEntity userList = userRepository.findById(user.getId()).get();
+
+            //Assertions
+            Assertions.assertThat(userList).isNotNull();
+
+        }
+
+
+
+
     }
 }
